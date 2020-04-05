1436fc5bcd43c7e12906cedbbd96f0d52f57f422
Connor Low <clow2@my.gcu.edu>
2020-04-05 13:33:00 -0700

    Golf

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export const setCookie = (name: string, value: any, expiresInDays: number = 1) => {
-    const d = new Date();
-    d.setTime(d.getTime() + (expiresInDays * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value};expires=${d.toUTCString()};path=/`;
+    document.cookie = `${name}=${value};expires=${(new Date(Date.now() + (expiresInDays * 24 * 60 * 60 * 1000))).toUTCString()};path=/`;
 }
 
 export const getCookie = (name: string) => (new RegExp(`(?:^|;\\s)${name.replace(/(\.)/g, '\\$1')}=([^;]+)(?:;\\s|$)`).exec(decodeURIComponent(document.cookie)) || [, ''])[1];
